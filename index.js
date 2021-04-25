a61cea1fe9515564c106eaef7d7a231e186a3219
Hede <Heatherj.swift@gmail.com>
2021-04-25 15:14:29 -0400

    get the first question to properly appear and not allow user to go further if not answered

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+//const generateReadme = require('./src/README-template');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'Welcome to the README generator! Please begin by entering the title of your project. (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title to continue!');
+                    return false;
+                }
+            }
+        }
+    ]);
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
+//questions();
