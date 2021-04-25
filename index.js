eaa0417a7d5459227ce801805163e408c5184f7f
Hede <Heatherj.swift@gmail.com>
2021-04-25 15:59:09 -0400

    get first set of prompts to correctly execute

--- index.js
+++ index.js
@@ -19,8 +19,68 @@ const promptUser = () => {
             }
         },
         {
-            type: 'input'
+            type: 'input',
+            name: 'description',
+            message: "Describe your project. If you're not sure what to write think about what, why, and how did you created this project. What was your motivation? Why did you build it? What problem does it solve? What did you learn? What makes your project stand out? (Required)",
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description! Something quick as a placeholder will do.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running. (Required)",
+            validate: installInput => {
+                if (installInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description! Something quick as a placeholder will do.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: "Provide instructions and examples for use. (Required)",
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description! Something quick as a placeholder will do.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmCredits',
+            message: "Would you like to add a credits section?",
+            default: true 
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: "List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.",
+            when: ({ confirmCredits }) => {
+                if (confirmCredits) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
+        // {
+        //     type: 'rawlist',
+        //     name: 'license',
+        //     message: 'What type of license is on your project?'
+        // }
+           
     ]);
 }
 
@@ -32,4 +92,4 @@ const promptUser = () => {
 
 // // Function call to initialize app
 // init();
-//promptUser();
+promptUser();
